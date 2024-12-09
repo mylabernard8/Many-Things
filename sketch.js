1ce4e7e14df847f594014dc70721206ec45010d8
mylabernard8 <myla.bernard26@avenues.org>
2024-12-09 07:34:24 -0500

    Update sketch.js with additions of drawing stars and positions

--- sketch.js
+++ sketch.js
@@ -1,9 +1,26 @@
+let stars = []; 
+
 function setup() {
   createCanvas(800, 600);
+  
+  //star positions
+  for (let i = 0; i < 10; i++) {
+    stars.push({
+      x: random(width),
+      y: random(height),
+      size: random(2, 5) // Random size for the stars
+    });
+  }
 }
 
 function draw() {
   background(0); // Black for the "night sky"
-  
+  // Drawing the stars
+  for (let star of stars) {
+    fill(255); // White color for the stars
+    noStroke();
+    ellipse(star.x, star.y, star.size);
+  }
 }
+
 
