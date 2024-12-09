4055596dac83abeda85b859e83785b60ca754f41
mylabernard8 <myla.bernard26@avenues.org>
2024-12-09 07:38:10 -0500

    Updating code with animation of the stars

--- sketch.js
+++ sketch.js
@@ -1,26 +1,32 @@
-let stars = []; 
-
 function setup() {
-  createCanvas(800, 600);
-  
-  //star positions
+  createCanvas(800, 600); 
+
   for (let i = 0; i < 10; i++) {
     stars.push({
       x: random(width),
       y: random(height),
-      size: random(2, 5) // Random size for the stars
+      size: random(2, 5),// Random size for the stars
+      speed: random(1, 3) // speed
     });
   }
 }
 
 function draw() {
-  background(0); // Black for the "night sky"
+  background(0);// Random size for the stars 
   // Drawing the stars
   for (let star of stars) {
-    fill(255); // White color for the stars
+    fill(255);// White color for the stars
     noStroke();
     ellipse(star.x, star.y, star.size);
+    
+    // Moving the star
+    star.y += star.speed;
+    
+    // Reset if it moves off the canvas
+    if (star.y > height) {
+      star.y = 0;
+      star.x = random(width);
+    }
   }
 }
-
 
