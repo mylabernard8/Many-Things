7964a0a1018dc6d1b1b87d654ab1c12dccd78f14
mylabernard8 <myla.bernard26@avenues.org>
2024-12-09 14:19:10 -0500

    Finalizing code with user interactions, listing below:
    - Creating a new star when you click the canvas in the spot you click it.
    
    - Changing all of the colors of the star once you click a key

--- sketch.js
+++ sketch.js
@@ -1,32 +1,36 @@
-let stars = []; // Array to hold star positions
+let stars = [];
 
 function setup() {
-  createCanvas(800, 600); 
+  createCanvas(800, 600);
 
+  
   for (let i = 0; i < 10; i++) {
     stars.push({
       x: random(width),
       y: random(height),
-      size: random(2, 5),// Random size for the stars
-      speed: random(1, 3) // speed
+      size: random(2, 5), // Random size
+      speed: random(1, 3),
+      color: color(255)
     });
   }
 }
 
 function draw() {
-  background(0); 
-  
+  background(0);
+
   for (let star of stars) {
     // Update size and brightness
-    star.size += sin(frameCount * 0.05) * 0.1; // Pulsating effect
+    star.size += sin(frameCount * 0.05) * 0.1; // pulsating effect
     let brightness = 200 + sin(frameCount * 0.05) * 55; // Twinkling effect
-    
-    fill(brightness);
+
+    fill(star.color.levels[0], star.color.levels[1], star.color.levels[2], brightness);
     noStroke();
     ellipse(star.x, star.y, star.size);
-    
+
+    // move the star
     star.y += star.speed;
 
+    // Reset star when it moves off the bottom edge
     if (star.y > height) {
       star.y = 0;
       star.x = random(width);
@@ -34,4 +38,21 @@ function draw() {
   }
 }
 
+// Add a new star where the mouse is clicked in teh spot where it is clicked
+function mousePressed() {
+  stars.push({
+    x: mouseX,
+    y: mouseY,
+    size: random(2, 5),
+    speed: random(1, 3),
+    color: color(255) // white is default
+  });
+}
+
+// change the color of all stars when a key is pressed
+function keyPressed() {
+  for (let star of stars) {
+    star.color = color(random(255), random(255), random(255)); // Random color
+  }
+}
 
